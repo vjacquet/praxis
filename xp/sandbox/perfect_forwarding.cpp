98688327bc1115fd24d1d4fdfeaa1a5f90ccc89b
vjacquet <vjacquet@flowgroup.fr>
2014-10-05 17:39:38 +0200

    Added perfect_forwarding sandbox.

---
+++ xp/sandbox/perfect_forwarding.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <type_traits>
+#include <utility>
+
+#include "../tests/testbench.h"
+
+namespace herbsutter {
+
+	class employee {
+		std::string name_;
+	public:
+		std::string const& get_name() const {
+			return name_;
+		}
+
+		template<class String, class = std::enable_if_t<std::is_same<std::decay_t<String>, std::string>::value>>
+		void set_name(String&& name) /*noexcept(std::is_nothrow_assignable<std::string&, String>::value)*/ {
+			name_ = std::forward<String>(name);
+		}
+	};
+
+}
+
+namespace my {
+
+	class employee {
+		std::string name_;
+	public:
+		const std::string& get_name() const {
+			return name_;
+		}
+
+		template<class String>
+		void set_name(String&& name) /*noexcept(std::is_nothrow_assignable<std::string&, String>::value)*/ {
+			name_ = std::forward<S>(name);
+		}
+	};
+}
+
+TESTBENCH()
+
+TEST(check_hs_employee) {
+	using namespace herbsutter;
+
+	std::string n = "test";
+
+	employee e;
+	e.set_name(n);
+	VERIFY_EQ(n, e.get_name());
+	//e.set_name("test");
+	//VERIFY_EQ(n, e.get_name());
+	e.set_name(std::string {"test"});
+	VERIFY_EQ(n, e.get_name());
+}
+
+TEST(check_my_employee) {
+	using namespace my;
+
+	std::string n = "test";
+
+	employee e;
+	e.set_name(n);
+	VERIFY_EQ(n, e.get_name());
+	e.set_name("test");
+	VERIFY_EQ(n, e.get_name());
+	e.set_name(std::string {"test"});
+	VERIFY_EQ(n, e.get_name());
+}
+
+TESTFIXTURE(perfect_forwarding)
