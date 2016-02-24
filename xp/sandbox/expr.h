71f95ade4b707e8d201a58abaac60a74552ab594
vjacquet <vjacquet@flowgroup.fr>
2016-02-24 21:24:11 +0100

    More fun with expr.

--- xp/sandbox/expr.h
+++ xp/sandbox/expr.h
@@ -6,6 +6,17 @@
 
 namespace xp {
 
+	//template<UnaryOperation Op, typename T>
+	//struct expr {
+	//	T const& t;
+	//};
+
+	//template<UnaryOperation Op, typename T>
+	//auto eval(expr<Op, T> const& e) {
+	//	Op op;
+	//	return op(eval(e.t));
+	//}
+
 	template<BinaryOperation Op, typename L, typename R>
 	struct expr {
 		L const& l;
@@ -15,7 +26,7 @@ namespace xp {
 	template<BinaryOperation Op, typename L, typename R>
 	auto eval(expr<Op, L, R> const& e) {
 		Op op;
-		return op(e.l, e.r);
+		return op(eval(e.l), eval(e.r));
 	}
 
 	template<typename T>
@@ -24,9 +35,30 @@ namespace xp {
 	}
 
 	template<typename L, typename R>
+	auto operator +(L const& l, R const& r)-> expr<std::plus<void>, L, R> {
+		return{ l,r };
+	}
+
+	template<typename L, typename R>
+	auto operator -(L const& l, R const& r)-> expr<std::minus<void>, L, R> {
+		return{ l,r };
+	}
+
+	template<typename L, typename R>
 	auto operator *(L const& l, R const& r)-> expr<std::multiplies<void>, L, R> {
 		return{ l,r };
 	}
+
+	template<typename L, typename R>
+	auto operator /(L const& l, R const& r)-> expr<std::divides<void>, L, R> {
+		return{ l,r };
+	}
+
+	//template<typename T>
+	//auto operator -(T const& t)-> expr<std::negate<void>, T> {
+	//	return{ t };
+	//}
+
 }
 
 #endif __EXPR_H__
